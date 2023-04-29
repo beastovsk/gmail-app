b1d83d7c01803ae21068bd76ea6064e5c380157d
Tim <TimF1234@yandex.ru>
2023-04-29 17:58:01 +0300

    added sidebar

---
+++ src/components/ui/ButtonGroup2/ButtonGroup2.vue
@@ -0,0 +1,38 @@
+<!-- Позже нужно переделать этот компонент -->
+
+<template lang="">
+    <n-space justify="center">
+      <n-button>
+        <template #icon>
+          <n-icon>
+              <book-outlined/>
+          </n-icon>
+        </template>
+      </n-button>
+      <n-button>
+        <template #icon>
+          <div class="circle"></div>
+        </template>
+      </n-button>
+    </n-space>
+</template>
+
+<script lang="ts">
+    import {
+        BookOutlined
+    } from '@vicons/antd'
+    export default {
+        components: {
+            BookOutlined
+        }
+    }
+</script>
+
+<style scoped>
+    .circle {
+        width: 10px;
+        height: 10px;
+        background-color: gold;
+        border-radius: 50px;
+    }
+</style>
---
+++ src/components/ui/MenuComponent/MenuComponent.vue
@@ -0,0 +1,64 @@
+<template lang="">
+    <n-space vertical>
+        <n-menu
+            v-model:value="activeKey"
+            :collapsed="false"
+            :collapsed-width="64"
+            :collapsed-icon-size="15"
+            :options="menuOptions"
+        />
+    </n-space>
+</template>
+
+<script lang="ts">
+    import { defineComponent, h, ref } from 'vue'
+    import type { Component } from 'vue'
+    import { NIcon } from 'naive-ui'
+    import type { MenuOption } from 'naive-ui'
+    import {
+        MailOutlined,
+        MailFilled,
+        DeleteOutlined,
+        CloseCircleOutlined,
+    } from '@vicons/antd'
+    function renderIcon(icon: Component) {
+        return () => h(NIcon, null, { default: () => h(icon) })
+    }
+
+    const menuOptions: MenuOption[] = [
+        {
+            label: 'Входящие',
+            key: 'Incoming',
+            icon: renderIcon(MailOutlined)
+        },
+        {
+            label: 'Рассылки',
+            key: 'Mailing',
+            icon: renderIcon(MailFilled)
+        },
+        {
+            label: 'Удалённые',
+            key: 'Deleted',
+            icon: renderIcon(DeleteOutlined)
+        },
+        {
+            label: 'Спам',
+            key: 'Spam',
+            icon: renderIcon(CloseCircleOutlined)
+        },
+    ]
+
+    export default defineComponent({
+        setup() {
+            return {
+                activeKey: ref<string | null>(null),
+                collapsed: ref(true),
+                menuOptions
+            }
+        }
+    })
+</script>
+
+<style lang="">
+    
+</style>
