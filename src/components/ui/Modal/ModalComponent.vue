30ba4c5be71cd0a55cdc89ac18b494da11c42310
Artyom Narshinov <narshinovartyom@mail.ru>
2023-04-30 20:08:47 +0400

    modal vuex state

--- src/components/ui/Modal/ModalComponent.vue
+++ src/components/ui/Modal/ModalComponent.vue
@@ -1,5 +1,5 @@
 <template lang="">
-  <n-modal :show="showModal">
+  <n-modal :show="this.$store.state.open">
     <n-card
       style="width: 600px"
       title="Modal"
@@ -7,6 +7,9 @@
       size="huge"
       role="dialog"
       aria-modal="true"
+      on-mask-click="this.$store.commit('setClose')"
+      :mask-closable="true"
+      :close-on-esc="true"
     >
       <template #header>
         <n-grid x-gap="12" :cols="5">
@@ -18,21 +21,15 @@
                 placeholder="Кому:"
                 :options="options"
               />
-              <n-input
-                placeholder="Тема:"
-              />
+              <n-input placeholder="Тема:" />
             </n-space>
           </n-grid-item>
 
           <n-grid-item span="1">
-            <n-button 
-              @click="closeModal"
-              quaternary 
-              circle
-            >
+            <n-button @click="$store.state.open = false" quaternary circle>
               <template #icon>
                 <n-icon>
-                  <close-outlined/>
+                  <close-outlined />
                 </n-icon>
               </template>
             </n-button>
@@ -49,61 +46,36 @@
           minRows: 3
         }"
       />
-      
+
       <template #footer>
-        <n-button>
-          Отправить
-        </n-button>
+        <n-button> Отправить </n-button>
       </template>
     </n-card>
   </n-modal>
 </template>
 
 <script>
-  import { ref } from 'vue'
-  import {
+import { CloseOutlined } from '@vicons/antd'
+
+export default {
+  components: {
     CloseOutlined
-  } from '@vicons/antd'
-
-  export default {
-    components: {
-      CloseOutlined,
-    },
-
-    props: {
-      showModal: Boolean
-    },
-
-    methods: {
-      showTest() {
-        this.$emit('showT', this.showModal)
-      },
-
-      closeModal() {
-        this.$emit('close-modal')
-      }
-    },
-
-    setup () {
-      return {
-        selectedValue: ref(null),
-        selectedValues: ref(null),
-        value: ref(null),
-        options: [
-          {
-            label: 'Email 1',
-            value: 'user1'
-          },
-          {
-            label: 'Email 2',
-            value: 'user2'
-          }
-        ]
-      }
+  },
+  setup() {
+    return {
+      options: [
+        {
+          label: 'Email 1',
+          value: 'user1'
+        },
+        {
+          label: 'Email 2',
+          value: 'user2'
+        }
+      ]
     }
   }
+}
 </script>
 
-<style lang="">
-    
-</style>
+<style lang=""></style>
--- src/store.ts
+++ src/store.ts
@@ -11,8 +11,25 @@ export const store = createStore({
           title: 'Кто-то входил в ваш аккаунт',
           description: 'Кто-то ввел неправильный пароль',
           date: '12 апреля'
+        },
+        {
+          id: 1,
+          image: '',
+          from: 'Яндекс ID',
+          title: 'Кто-то входил в ваш аккаунт',
+          description: 'Кто-то ввел неправильный пароль',
+          date: '24 апреля'
         }
-      ]
+      ],
+      open: false
+    }
+  },
+  mutations: {
+    setOpen() {
+      store.state.open = true
+    },
+    setClose() {
+      store.state.open = false
     }
   }
 })
