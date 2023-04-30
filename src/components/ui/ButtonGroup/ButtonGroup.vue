3792b07e93291e6e68bc228b98f23be3d43b7c9b
Tim <TimF1234@yandex.ru>
2023-04-30 16:13:15 +0300

    added modal with sending a message

--- src/components/ui/ButtonGroup/ButtonGroup.vue
+++ src/components/ui/ButtonGroup/ButtonGroup.vue
@@ -1,12 +1,16 @@
 <template lang="">
   <n-space justify="center">
-    <n-button round type="warning">
-        <template #icon>
-            <n-icon>
-                <send-outlined/>
-            </n-icon>
-        </template>
-        Написать
+    <n-button 
+      round 
+      type="warning"
+      @click="showModal = true"
+    >
+      <template #icon>
+          <n-icon>
+              <send-outlined/>
+          </n-icon>
+      </template>
+      Написать
     </n-button>
     <n-button circle type="warning">
       <template #icon>
@@ -16,19 +20,39 @@
       </template>
     </n-button>
   </n-space>
+
+  <modal-component 
+    :show="showModal" 
+    @close-modal="showModal = false" 
+  />
+  
 </template>
 
 <script lang="ts">
-    import {
-        ReloadOutlined,
-        SendOutlined,
-    } from '@vicons/antd'
-    export default {
-        components: {
-            ReloadOutlined,
-            SendOutlined,
-        }
-    }
+  import ModalComponent from '../Modal/ModalComponent.vue'
+  import { defineComponent, ref } from 'vue'
+  import {
+      ReloadOutlined,
+      SendOutlined,
+  } from '@vicons/antd'
+
+  export default defineComponent ({
+    components: {
+      ReloadOutlined,
+      SendOutlined,
+      ModalComponent,
+    },
+
+    setup () {
+      const showModal = ref(false)
+
+      return {
+        showModal,
+      }
+    },
+  })
+
+
 </script>
 
 <style lang="">
---
+++ src/components/ui/Modal/ModalComponent.vue
@@ -0,0 +1,109 @@
+<template lang="">
+  <n-modal :show="showModal">
+    <n-card
+      style="width: 600px"
+      title="Modal"
+      :bordered="false"
+      size="huge"
+      role="dialog"
+      aria-modal="true"
+    >
+      <template #header>
+        <n-grid x-gap="12" :cols="5">
+          <n-grid-item span="4">
+            <n-space vertical>
+              <n-select
+                v-model:value="selectedValue"
+                filterable
+                placeholder="Кому:"
+                :options="options"
+              />
+              <n-input
+                placeholder="Тема:"
+              />
+            </n-space>
+          </n-grid-item>
+
+          <n-grid-item span="1">
+            <n-button 
+              @click="closeModal"
+              quaternary 
+              circle
+            >
+              <template #icon>
+                <n-icon>
+                  <close-outlined/>
+                </n-icon>
+              </template>
+            </n-button>
+          </n-grid-item>
+        </n-grid>
+      </template>
+
+      <n-input
+        v-model:value="value"
+        type="textarea"
+        placeholder="Напишите что-нибудь"
+        bordered="false"
+        :autosize="{
+          minRows: 3
+        }"
+      />
+      
+      <template #footer>
+        <n-button>
+          Отправить
+        </n-button>
+      </template>
+    </n-card>
+  </n-modal>
+</template>
+
+<script>
+  import { ref } from 'vue'
+  import {
+    CloseOutlined
+  } from '@vicons/antd'
+
+  export default {
+    components: {
+      CloseOutlined,
+    },
+
+    props: {
+      showModal: Boolean
+    },
+
+    methods: {
+      showTest() {
+        this.$emit('showT', this.showModal)
+      },
+
+      closeModal() {
+        this.$emit('close-modal')
+      }
+    },
+
+    setup () {
+      return {
+        selectedValue: ref(null),
+        selectedValues: ref(null),
+        value: ref(null),
+        options: [
+          {
+            label: 'Email 1',
+            value: 'user1'
+          },
+          {
+            label: 'Email 2',
+            value: 'user2'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="">
+    
+</style>
